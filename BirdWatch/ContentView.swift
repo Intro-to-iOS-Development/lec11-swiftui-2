31648115e950e8d9981bcb638d15d6c7eedbf2ed
Vin Bui <vdb23@cornell.edu>
2024-03-18 23:14:22 -0400

    Finish Button

--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State private var count: Int = 0
+
     var body: some View {
         NavigationStack {
             VStack(spacing: 50) {
                 profileView
 
-                NavigationLink {
-                    BirdListView()
-                } label: {
-                    viewBirdsButton
+                HStack {
+                    addCountButton
+
+                    NavigationLink {
+                        BirdListView()
+                    } label: {
+                        viewBirdsButton
+                    }
                 }
             }
             .navigationTitle("Bird Watch")
@@ -33,7 +39,7 @@ struct ContentView: View {
             Group {
                 Text("Name: ")
                 Text("Location: ")
-                Text("Count: ")
+                Text("Count: \(count)")
             }
             .font(.title)
         }
@@ -46,6 +52,19 @@ struct ContentView: View {
             .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
             .background(.blue)
             .clipShape(RoundedRectangle(cornerRadius: 15))
+    }
+
+    private var addCountButton: some View {
+        Button {
+            count += 1
+        } label: {
+            Text("Add Count")
+                .font(.title2)
+                .foregroundColor(.white)
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .background(.green)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+        }
     }
 
 }
